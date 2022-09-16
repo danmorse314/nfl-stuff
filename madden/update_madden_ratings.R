7d0cdb651f7c5e6caed5ef599148c53c72a1461a
Dan Morse <44938476+danmorse314@users.noreply.github.com>
2022-09-16 08:48:00 -0700

    Update update_madden_ratings.R

--- madden/update_madden_ratings.R
+++ madden/update_madden_ratings.R
@@ -1,6 +1,6 @@
 source("madden/get_madden_ratings.R")
 
-ratings <- get_madden_ratings(update = "yes")
+ratings <- get_madden_ratings(update = TRUE)
 
 ratings |>
   readr::write_csv("madden/madden_23_player_ratings.csv")
