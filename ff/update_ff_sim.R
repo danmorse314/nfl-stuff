c083112623bde273d3fd565376273522c222a9d2
Dan Morse <44938476+danmorse314@users.noreply.github.com>
2025-03-18 09:19:37 -0700

    Update update_ff_sim.R

--- ff/update_ff_sim.R
+++ ff/update_ff_sim.R
@@ -12,7 +12,7 @@ year <- as.numeric(substr(Sys.Date(),1,4))
 nsims <- 1000
 
 # are schedules released for the sim year?
-actual_schedule <- FALSE
+actual_schedule <- F
 
 # league ID
 #lid <- "999807305069699072"  # 2023
